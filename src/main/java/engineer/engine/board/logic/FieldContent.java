bb2f5ff98239ee1df33049afdc9710326c8ff6ca
Andrei Daletski <andrey8daletsky@gmail.com>
2022-05-07 23:48:14 +0200

    Removed old test implementation

--- src/main/java/engineer/engine/board/logic/FieldContent.java
+++ src/main/java/engineer/engine/board/logic/FieldContent.java
@@ -4,5 +4,4 @@ package engineer.engine.board.logic;
 public interface FieldContent {
     // Represent any type of entity that can be in the field e.g. building, troops
     // Also stores info about textures or animations
-    boolean canBuild();
 }
