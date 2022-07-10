a5e1c8038efb1a3e825bb9efd68a3b7629585dc2
Andrei Daletski <andrey8daletsky@gmail.com>
2022-07-10 23:23:46 +0300

    Add Menu

--- src/main/java/engineer/gui/javafx/Gui.java
+++ src/main/java/engineer/gui/javafx/Gui.java
@@ -1,103 +1,78 @@
 package engineer.gui.javafx;
 
-import javafx.application.Platform;
+import engineer.engine.board.logic.Board;
+import engineer.engine.board.logic.BoardDescription;
+import engineer.engine.board.logic.FieldFactoryImpl;
+import engineer.engine.board.presenter.BoardPresenter;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
+
+import static engineer.gui.javafx.GameGui.title;
 
 public class Gui {
-    private static final String title = "The Engineer";
-    private static final int windowWidth = 1080;
-    private static final int windowHeight = 720;
+    private static Stage window;
+    private final int windowWidth = 1080;
+    private final int windowHeight = 720;
 
-    private BoardGui boardGui;
+    public void start() {
+        if (window == null) {
+            window = new Stage();
+        }
+        window.setTitle(title);
+        window.setResizable(false);
 
-    public void start(Runnable runnable) {
-        Platform.startup(() -> {
-            Stage window = new Stage();
-            window.setTitle(title);
-            window.setResizable(false);
+        VBox vbox = new VBox();
+        vbox.setAlignment(Pos.CENTER);
+        vbox.setSpacing(5.);
 
-            Canvas canvas = new Canvas(windowWidth, windowHeight);
+        Image exitImg = new Image("file:src/main/resources/images/exit.png");
+        ImageView exitImgView = new ImageView(exitImg);
+        exitImgView.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> window.close());
 
-            Button button = new Button("house");
-            button.setFocusTraversable(false);
-            button.setId("house");
-            button.setLayoutX(20);
-            button.setLayoutY(20);
+        Image startGameImg = new Image("file:src/main/resources/images/startGame.png");
+        ImageView startGameImgView = new ImageView(startGameImg);
+        startGameImgView.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> startGame());
 
-            Button button2 = new Button("house2");
-            button2.setFocusTraversable(false);
-            button2.setId("house2");
-            button2.setLayoutX(20);
-            button2.setLayoutY(60);
+        vbox.getChildren().addAll(startGameImgView, exitImgView);
 
-            // TODO: change to TextureManager
-            Image pauseImg = new Image("file:src/main/resources/images/pause.png");
-            ImageView pauseImgView = new ImageView(pauseImg);
 
-            AnchorPane root = new AnchorPane();
+        StackPane root = new StackPane();
 
-            Image pauseTextImg = new Image("file:src/main/resources/images/pauseText.png");
-            ImageView pauseTextImgView = new ImageView(pauseTextImg);
-            pauseTextImgView.setVisible(false);
+        Image backgroundImg = new Image("file:src/main/resources/images/startBackground.png");
+        ImageView backgroundImgView = new ImageView(backgroundImg);
+        backgroundImgView.setFitWidth(windowWidth);
+        backgroundImgView.setFitHeight(windowHeight);
 
-            boolean[] pausing = new boolean[1];
-            pauseImgView.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
-                pausing[0] = !pausing[0];
-                if (pausing[0]) {
-                    button.setDisable(true);
-                    button2.setDisable(true);
-                    pauseTextImgView.setVisible(true);
-                    //   As soon as new buttons will appear, add them here
-                } else {
-                    button.setDisable(false);
-                    button2.setDisable(false);
-                    pauseTextImgView.setVisible(false);
-                    // and here
-                }
-                System.out.println("Pause pressed");
-                mouseEvent.consume();
-            });
+        root.getChildren().addAll(backgroundImgView, vbox);
 
-            root.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
-                if (pausing[0]) {
-                    mouseEvent.consume();
-                }
-            });
 
-            // TODO: if we want to make windows resizable, think about binding the width and the height (just callbacks)
-            AnchorPane.setTopAnchor(pauseTextImgView, canvas.getHeight() / 2);
-            AnchorPane.setLeftAnchor(pauseTextImgView, canvas.getWidth() / 2 - pauseTextImg.getWidth() / 2);
-
-            AnchorPane.setTopAnchor(pauseImgView, 5.);
-            AnchorPane.setLeftAnchor(pauseImgView, canvas.getWidth() / 2 - pauseImg.getWidth() / 2);
-
-            root.getChildren().addAll(canvas, pauseTextImgView, pauseImgView, button, button2);
-
-            window.setScene(new Scene(root));
-            window.show();
-            boardGui = new BoardGui(canvas.getGraphicsContext2D());
-            window.addEventHandler(WindowEvent.WINDOW_CLOSE_REQUEST, event -> boardGui.close());
-            window.getScene().setOnKeyPressed(e -> boardGui.getKeyHandler().handleKey(e.getCode(), true));
-            window.getScene().setOnKeyReleased(e -> boardGui.getKeyHandler().handleKey(e.getCode(), false));
-
-            window.getScene().setOnMouseClicked(boardGui.getOnFieldClickHandler());
-
-            button.setOnAction(boardGui.getButtonClickedHandler());
-            button2.setOnAction(boardGui.getButtonClickedHandler());
-
-            runnable.run();
-        });
+        window.setScene(new Scene(root, windowWidth, windowHeight));
+        window.show();
     }
 
-    public BoardGui getBoardGui() {
-        return boardGui;
+    public void startGame() {
+        Board board = new Board(new FieldFactoryImpl(), new BoardDescription() {
+            @Override
+            public int getRows() { return 40; }
+            @Override
+            public int getColumns() { return 50; }
+            @Override
+            public String getBackground(int row, int column) {
+                return "tile";
+            }
+        });
+
+        // Sample
+        GameGui gameGui = new GameGui();
+        gameGui.start(() -> {
+            BoardPresenter boardPresenter = new BoardPresenter(board, gameGui.getBoardGui());
+            gameGui.getBoardGui().start(boardPresenter);
+        }, window, this::start);
     }
 }
