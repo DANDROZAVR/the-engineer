5c1f3fa457e413ffa9660a39576fad5fa469cccd
Hubert Zięba <hub.zieba@student.uj.edu.pl>
2022-07-14 19:27:58 +0200

    Change mouse handling

--- src/main/java/engineer/gui/javafx/GameGui.java
+++ src/main/java/engineer/gui/javafx/GameGui.java
@@ -75,14 +75,13 @@ public class GameGui {
         gameScene = new Scene(root);
 
         boardGui = new BoardGui(canvas.getGraphicsContext2D(), textureManager);
-        root.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
-            if (!pausing) {
-                boardGui.getOnFieldClickHandler().handle(mouseEvent);
-            }
-            mouseEvent.consume();
+        root.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> {
+            if (!pausing)
+                boardGui.onFieldClicked(e.getX(), e.getY());
+            e.consume();
         });
-        button.setOnAction(boardGui.getButtonClickedHandler());
-        button2.setOnAction(boardGui.getButtonClickedHandler());
+        button.setOnAction(e -> boardGui.onButtonClicked(((Button) e.getTarget()).getId()));
+        button2.setOnAction(e -> boardGui.onButtonClicked(((Button) e.getTarget()).getId()));
         pauseImgView.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
             pausing = !pausing;
             if (pausing) {
@@ -106,7 +105,7 @@ public class GameGui {
         window.setResizable(false);
         window.setScene(gameScene);
 
-        window.getScene().setOnMouseClicked(boardGui.getOnFieldClickHandler());
+        window.getScene().setOnMouseClicked(e -> boardGui.onFieldClicked(e.getX(), e.getY()));
         window.getScene().setOnKeyPressed(e -> {
             if (!pausing)
                 boardGui.getKeyHandler().handleKey(e.getCode(), true);
